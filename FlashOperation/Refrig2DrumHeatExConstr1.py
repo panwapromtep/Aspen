aa6e5cf8dba9e4fb3d02865d3d6d1dc3d5c9bdb9
panwapromtep <panwapromtep@gmail.com>
2025-04-21 02:06:22 -0400

    added anew class for NN

--- FlashOperation/Refrig2DrumHeatExConstr1.py
+++ FlashOperation/Refrig2DrumHeatExConstr1.py
@@ -97,6 +97,83 @@ class Refrig2DrumConstraintHeatExConstr(Refrig2Drum2Comp):
         cost = self.costFunc(results)
         return cost
     
+#overload only the cost function to include the constraint on temperature
+class Refrig2DrumConstraintHeatExConstrforNN(Refrig2Drum2Comp):
+    def __init__(self, AspenFile, wdpath, visibility = False):
+        super().__init__(AspenFile, wdpath, visibility)
+        self.open_simulation()
+        self.cpu_usage = []
+        self.memory_usage = []
+        self.timestamps = []
+        self.start_time = time.time()
+        
+    @staticmethod
+    def flatten_params(x_dict):
+        flat_list = []
+        for block_type in ["Flash2"]:
+            for block, params in x_dict[block_type].items():
+                flat_list.extend(params)
+        return np.array(flat_list)
+
+    @staticmethod
+    def unflatten_params(flat_array):
+        x_dict = {
+            "Flash2": {"FLASH1": [flat_array[0]], "FLASH2": [flat_array[1]]},
+        }
+        return x_dict
+        
+    def sample_usage(self):
+        self.cpu_usage.append(psutil.cpu_percent(interval=None))
+        self.memory_usage.append(psutil.virtual_memory().used / (1024 * 1024))  # Convert to MB
+        self.timestamps.append(time.time() - self.start_time)
+        
+    def runSim(self, x):
+        self.open_simulation()
+        for blockname, params in x["Flash2"].items():
+            self.sim.BLK_FLASH2_Set_Pressure(blockname, params[0])
+
+        self.sim.DialogSuppression(True)
+        self.sim.Run()
+
+        results = {
+            "OUTF1_MF": self.sim.STRM_Get_MoleFlow("OUTF1"),
+            "OUTF2_MF": self.sim.STRM_Get_MoleFlow("OUTF2"),
+            "OUTF1_H": self.sim.STRM_Get_Molar_Enthalpy("OUTF1"),
+            "OUTF2_H": self.sim.STRM_Get_Molar_Enthalpy("OUTF2"),
+            "OUTCOMP1_H": self.sim.STRM_Get_Molar_Enthalpy("OUTCOMP1"),
+            "OUTCOMP2_H": self.sim.STRM_Get_Molar_Enthalpy("OUTCOMP2"),
+            "TEMPOUT": self.sim.STRM_Get_Temperature("OUTSTR")
+        }
+    
+        
+        return results
+    
+    def costFunc(self, results):
+        FLOW_1 = results["OUTF1_MF"] # kmol/hr
+        FLOW_2 = results["OUTF2_MF"] # kmol/hr
+        OUTCOMP1 = results["OUTCOMP1_H"] #cal/sec
+        OUTF1 = results["OUTF1_H"]  #cal/sec
+        OUTCOMP2 = results["OUTCOMP2_H"]  #cal/sec
+        OUTF2 = results["OUTF2_H"] #cal/sec
+        TEMPOUT = results["TEMPOUT"] # Celcius
+        
+        if FLOW_1 == 0:
+            OUTF1 = OUTCOMP1 = 0
+        if FLOW_2 == 0:
+            OUTF2 = OUTCOMP2 = 0
+
+        cost = (1000 * FLOW_1 * ((OUTCOMP1 - OUTF1) / 0.65) + \
+            1000 * FLOW_2 * ((OUTCOMP2 - OUTF2) / 0.65))/4184
+                
+        print(f"TEMPOUT: {TEMPOUT}")
+        return cost
+
+    def run_obj(self, x_dict):
+        results = self.runSim(x_dict)
+        cost = self.costFunc(results)
+        return [cost, results["TEMPOUT"]]
+    
+    
     
 def main():
     print("Starting Refrig2DrumConstraintHeatExConstr simulation.")
